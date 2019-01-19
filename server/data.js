7bd7e37d6973f4e230dd8a9e817706faaadd6761
jpcastberg <jpcastberg@gmail.com>
2019-01-19 10:20:44 -0800

    Re-adds index.html and data.js

---
+++ server/data.js
@@ -0,0 +1,11 @@
+var messages = {};
+messages.results = [];
+
+var testMsg = {
+  username : 'JohnKevin',
+  roomname : 'lobby',
+  text : 'test message yay!'
+};
+messages.results.push(testMsg);
+
+exports.messages = messages;
