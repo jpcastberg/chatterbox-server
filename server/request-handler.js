95f0bff258c04260a93af59df12a0d06af8f29fd
jpcastberg <jpcastberg@gmail.com>
2019-01-18 21:05:57 -0800

    Implement basic server functionality, debugging still req'd

--- server/request-handler.js
+++ server/request-handler.js
@@ -29,6 +29,7 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
+  //console.log(request);
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
   // The outgoing status.
@@ -48,6 +49,31 @@ var requestHandler = function(request, response) {
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
   headers['Content-Type'] = 'application/json';
+  
+  if (request.method === 'POST') {
+    statusCode = 201;
+    let body = [];
+
+    request.on('error', (err) => {
+      console.error(err);
+    }).on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      //console.log(body);
+      body = JSON.parse(body);
+      data.messages.results.push(body);
+      console.log(data.messages.results);
+    });
+    //call newMessage method on data.js
+  }
+
+  if (request.method === 'GET' && request.url === '/?order=-createdAt') {
+    statusCode = 200;
+    response.writeHead(statusCode, headers);
+    console.log(JSON.stringify(data.messages));
+    response.end(JSON.stringify(data.messages));
+  }
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
@@ -60,7 +86,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end(JSON.stringify({results: []}));
+  response.end();
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
